7b06f42f3d53016dce642a1fb03cea3d8c0336c8
Reece <reecemorgandev@gmail.com>
2023-03-03 21:45:05 +0000

    removed console.log

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,6 @@ import { getGenerations } from "./+server";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-    console.log('get the generations')
     const res = await getGenerations();
     console.log('res: ' + JSON.stringify(res));
     return res;
