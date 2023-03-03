830d061e673efe7c76439781383f59826e829438
Reece <reecemorgandev@gmail.com>
2023-03-03 21:24:00 +0000

    added coming soon page

---
+++ src/routes/coming-soon/+page.svelte
@@ -0,0 +1,44 @@
+<div class="content">
+    <img src="/ComingSoon.png" alt="Content coming soon!" />
+    <p>Return to the <a href='/'>Home Page</a></p>
+</div>
+
+<div class="poke-images">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" alt="Bulbasuar" />
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png" alt="Charmander" />
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/7.png" alt="Squirtle" />
+</div>
+
+<style>
+    .content {
+        width: 500px;
+        margin: 0 auto;
+    }
+
+    .content p{
+        font-size: 2em;
+        font-family: 'Aldrich', 'Verdana', sans-serif;
+        text-align: center;
+    }
+
+    .content a {
+        color: #ffcb05;
+        font-weight: 800;
+        text-decoration: none;
+    }
+
+    .content a:hover {
+        color: #ffcb05;
+        text-decoration: underline;
+    }
+    
+    .poke-images {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .poke-images img {
+        width: 30%;
+    }
+</style>
