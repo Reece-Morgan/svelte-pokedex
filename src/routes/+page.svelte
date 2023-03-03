6adf0b519b1738478f45e58e0dba0206f3022ed9
Reece <reecemorgandev@gmail.com>
2023-03-03 21:06:05 +0000

    added content to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { formatText, getRegionIcon } from './+server';
+    import '../styles/fonts.css';
+
+    /** @type {import('./$types').PageData} */  
+    export let data;
+</script>
+
+<h2 class="title">Browse through the Pokedex by generation!</h2>
+
+<div class="gen-grid">
+    {#each data.results as result}
+        <div class="gen-tile">
+            <img src={getRegionIcon(result.name)} alt={result.name} />
+            <p>{formatText(result.name)}</p>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .title {
+        color: #ffcb05;
+        font-size: 2em;
+        font-weight: 800;
+        font-family: 'Aldrich', 'Verdana', sans-serif;
+        text-align: center;
+    }
+
+    .gen-grid {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        max-width: 1000px;
+        margin: 0 auto;
+    }
+
+    .gen-tile {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 0 10px;
+    }
+
+    .gen-grid .gen-tile img {
+        height: 300px;
+    }
+
+    .gen-grid .gen-tile p {
+        color: #ffcb05;
+        font-size: 1em;
+        font-weight: 500;
+        font-family: 'Aldrich', 'Verdana', sans-serif;
+    }
+</style>
