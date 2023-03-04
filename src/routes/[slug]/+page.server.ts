128220283c145b0365ba0f060f7c1819a28d8d96
Reece <reecemorgandev@gmail.com>
2023-03-04 19:49:47 +0000

    added checks to slug urls

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,11 +1,29 @@
+import { error } from "@sveltejs/kit";
 import { getLimitAndOffset, getPokemon } from "./+server";
 
 /** @type {import('./$types').PageServerLoad} */
 export const load = async ({ params }) => {
     const param = getLimitAndOffset(params.slug);
     const res = await getPokemon(param.limit, param.offset);
-    return {
-        res: res,
-        slug: params.slug
+    if (acceptableUrls.includes(params.slug)) {
+        console.log(params.slug + ' is an acceptable url');
+        return {
+            res: res,
+            slug: params.slug
+        }
+    } else {
+        throw error(404, 'Not found');
     }
 }
+
+const acceptableUrls = [
+    'generation-i',
+    'generation-ii',
+    'generation-iii',
+    'generation-iv',
+    'generation-v',
+    'generation-vi',
+    'generation-vii',
+    'generation-viii',
+    'generation-ix'
+]
