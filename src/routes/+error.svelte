ea7d70b41ab923a9c64bf80854deec5ce4473f5c
Reece <reecemorgandev@gmail.com>
2023-03-04 19:49:19 +0000

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,59 @@
+<div class="content">
+    <img src="/404.png" alt="404: Page Not Found" />
+    <p>Return to the <a href='/'>Home Page</a></p>
+</div>
+
+<div class="poke-images">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" alt="Bulbasuar" />
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png" alt="Charmander" />
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/7.png" alt="Squirtle" />
+</div>
+
+<style>
+    .content {
+        width: 600px;
+        margin: 0 auto;
+        padding: 20px 0 0 0;
+    }
+
+    .content img {
+        max-width: 100%
+    }
+
+    .content p {
+        font-size: 2em;
+        font-family: 'Aldrich', 'Verdana', sans-serif;
+        text-align: center;
+    }
+
+    .content a {
+        color: #ffcb05;
+        font-weight: 800;
+        text-decoration: none;
+    }
+
+    .content a:hover {
+        color: #ffcb05;
+        text-decoration: underline;
+    }
+    
+    .poke-images {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .poke-images img {
+        width: 30%;
+    }
+
+    @media(max-width: 500px) {
+        .content {
+            max-width: 320px;
+        }
+        
+        .content img {
+            max-width: 320px;
+        }
+    }
+</style>
